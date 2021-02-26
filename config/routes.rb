c840beace67b63782a1b1c84600484c2f9e5099b
Alex Smirnov <sas2job@gmail.com>
2021-02-26 11:19:01 +0300

    Add Post model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  scope module: 'authors' do
+    resources :posts
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_074056) do
+ActiveRecord::Schema.define(version: 2021_02_26_075725) do
 
   create_table "authors", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +24,16 @@ ActiveRecord::Schema.define(version: 2021_02_26_074056) do
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "published"
+    t.datetime "published_at"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "posts", "authors"
 end
